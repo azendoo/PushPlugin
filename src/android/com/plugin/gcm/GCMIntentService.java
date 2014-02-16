928c4e3565113fcbc7ae5bee64bc2fe344c2d600
Pierre Renaudin <pierre.renaudin.fr@gmail.com>
2014-02-16 17:50:26 +0100

    added a large icon to the application default icon

--- src/android/com/plugin/gcm/GCMIntentService.java
+++ src/android/com/plugin/gcm/GCMIntentService.java
@@ -1,23 +1,21 @@
 package com.plugin.gcm;
 
-import com.azendoo.azmobile.R;
-import java.util.List;
-
-import com.google.android.gcm.GCMBaseIntentService;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
-import android.app.ActivityManager;
-import android.app.ActivityManager.RunningTaskInfo;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+
+import com.azendoo.azmobile.R;
+import com.google.android.gcm.GCMBaseIntentService;
 
 @SuppressLint("NewApi")
 public class GCMIntentService extends GCMBaseIntentService {
@@ -99,6 +97,7 @@ public class GCMIntentService extends GCMBaseIntentService {
 			new NotificationCompat.Builder(context)
 				.setDefaults(Notification.DEFAULT_ALL)
 				.setSmallIcon(R.drawable.ic_stat_notify)
+				.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), context.getApplicationInfo().icon))
 				.setWhen(System.currentTimeMillis())
 				.setContentTitle(extras.getString("title"))
 				.setTicker(extras.getString("title"))
